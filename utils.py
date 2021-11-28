061395ce20a322388f369c4dc3b4365711d624ef
DKumar_99 <dibyakantikumar1999@gmail.com>
2021-11-28 21:54:07 +0530

    add the utils file

---
+++ utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import os
+import torch
+import time, sys
+from transformers import AlbertForQuestionAnswering,AlbertTokenizerFast,AutoTokenizer,AutoModelForQuestionAnswering,AutoModel,XLMRobertaForQuestionAnswering,XLMRobertaTokenizerFast,BertTokenizerFast,BertForQuestionAnswering
+
+class chaiDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings):
+        self.encodings = encodings
+
+    def __getitem__(self, idx):
+        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+
+    def __len__(self):
+        return len(self.encodings.input_ids)
+
+def preprocess(tokenizer,data):
+	encodings = tokenizer(list(data["context"]), list(data["question"]), truncation=True, padding=True)
+
+    start_positions = []
+    end_positions = []
+    id_ = []
+    for i in range(len(data["answer_start"])):
+        start_positions.append(encodings.char_to_token(i,data["answer_start"][i]))
+        end_positions.append(encodings.char_to_token( i,(data["answer_start"][i] + len(data['answer_text'][i]) - 1) ))
+        
+        # if start position is None, the answer passage has been truncated
+        if start_positions[-1] is None:
+            start_positions[-1] = tokenizer.model_max_length
+        if end_positions[-1] is None:
+            end_positions[-1] = tokenizer.model_max_length
+        
+        
+    encodings.update({'start_positions': start_positions, 'end_positions': end_positions})
+    
+    return encodings
